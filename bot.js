5c8c48fe1a52138b694101212db8752aef5eb688
yowzah <nagiev2002@bk.ru>
2021-09-10 01:36:23 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,14 +50,7 @@ bot.start((ctx) => {
     ctx.telegram.sendMessage(chatID, welcome, Extra.markdown())
   }
 })
-bot.on('text', (ctx) => {
-  if (ctx.chat.type === private){
-    ctx.telegram.forwardMessage(chatID, ctx.chat.id, ctx.message.message_id)
-  }
-  else if (ctx.chat.id === chatID) {
-    ctx.telegram.sendMessage(ctx.message.reply_to_message.forward_from.id, ctx.message.text)
-  }
-})
+
 bot.action('enterDebug', (ctx) => ctx.scene.enter('debug'))
 bot.action('enterEcho', (ctx) => ctx.scene.enter('echo'))
 bot.launch()
