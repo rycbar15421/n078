bda27f766488b7548849d538ee2a3756220db183
yowzah <nagiev2002@bk.ru>
2021-09-09 21:10:24 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,15 @@
 const { Scenes, session, Telegraf, Markup } = require('telegraf')
+const session = require('telegraf/session')
+const Stage = require('telegraf/stage')
+const Scene = require('telegraf/scenes/base')
+const Extra = require('telegraf/extra')
+const Markup = require('telegraf/markup')
+
+const Telegraf = require('telegraf')
+
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-const dashboard = Markup.inlineKeyboard([Markup.button.callback('Режим: Echo', 'echo')])
+const dashboard = Markup.inlineKeyboard([Markup.callbackButton('Режим: Echo', 'echo')])
 bot.action('echo', (ctx) => ctx.scene.enter('echo'))
 
 
@@ -13,22 +21,17 @@ bot.start((ctx) => {
   }
 })
 
-const { enter, leave } = Scenes.Stage
+const { enter, leave } = Stage
 
-const echoScene = new Scenes.BaseScene<Scenes.SceneContext>('echo')
+const echoScene = new Scene('echo')
 echoScene.enter((ctx) => ctx.reply('echo scene'))
 echoScene.leave((ctx) => ctx.reply('exiting echo scene'))
+echoScene.command('back', leave())
 echoScene.on('text', (ctx) => ctx.reply(ctx.message.text))
 echoScene.on('message', (ctx) => ctx.reply('Only text messages please'))
 
-const stage = new Scenes.Stage<Scenes.SceneContext>([echoScene], {
-  ttl: 10,
-})
+const stage = new Stage([greeterScene, echoScene], { ttl: 10 })
 bot.use(session())
 bot.use(stage.middleware())
 
 bot.launch()
-
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
