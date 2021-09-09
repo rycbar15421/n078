c64aef4ad9d69d8bb949d77b801edb4b33e17286
yowzah <nagiev2002@bk.ru>
2021-09-09 21:01:42 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
 const { Scenes, session, Telegraf, Markup } = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-const keyboard = Markup.inlineKeyboard([Markup.button.callback('Delete', 'delete')])
-bot.action('delete', (ctx) => ctx.deleteMessage())
-
 const dashboard = Markup.inlineKeyboard([Markup.button.callback('Режим: Echo', 'echo')])
 bot.action('echo', (ctx) => ctx.scene.enter('echo'))
 
@@ -21,7 +18,6 @@ const { enter, leave } = Scenes.Stage
 const echoScene = new Scenes.BaseScene<Scenes.SceneContext>('echo')
 echoScene.enter((ctx) => ctx.reply('echo scene'))
 echoScene.leave((ctx) => ctx.reply('exiting echo scene'))
-echoScene.command('back', leave<Scenes.SceneContext>())
 echoScene.on('text', (ctx) => ctx.reply(ctx.message.text))
 echoScene.on('message', (ctx) => ctx.reply('Only text messages please'))
 
@@ -31,7 +27,6 @@ const stage = new Scenes.Stage<Scenes.SceneContext>([greeterScene, echoScene], {
 bot.use(session())
 bot.use(stage.middleware())
 
-bot.on('message', (ctx) => ctx.telegram.sendCopy(ctx.message.chat.id, ctx.message, keyboard))
 bot.launch()
 
 // Enable graceful stop
