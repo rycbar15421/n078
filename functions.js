6a78424277d5b3907acb56b493545b023f6e18f2
yowzah <nagiev2002@bk.ru>
2021-09-11 19:51:46 +0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -5,9 +5,11 @@ const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
 const Telegraf = require('telegraf')
 
-export function dashboard() {
+function dashboard() {
     return Markup.inlineKeyboard([
       Markup.callbackButton('Режим: Echo', 'enterEcho'),
       Markup.callbackButton('Режим: Debug', 'enterDebug')
     ]).extra()	
-} 
+}
+
+module.exports = { dashboard } 
