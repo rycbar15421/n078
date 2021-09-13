93c71c21952d3480d14e30f82411e15c1da2504c
yowzah <nagiev2002@bk.ru>
2021-09-14 00:51:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const { Telegraf } = require('telegraf')
+const fs = require('fs')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.on('message', (ctx) => ctx.reply('Привет'))
-bot.launch({
-  webhook: {
-    domain: 'https://n078.herokuapp.com/',
-    port: process.env.PORT
-  }
-})
+bot.launch()
+// Set telegram webhook
+// The second argument is necessary only if the client uses a self-signed
+// certificate. Including it for a verified certificate may cause things to break.
+bot.telegram.setWebhook('https://n078.herokuapp.com/secret-path')
+
+
+// Http webhook, for nginx/heroku users.
+bot.startWebhook('/secret-path', null, 5000)
