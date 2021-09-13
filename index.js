40f42f1eec78a561f4dacd77cff71a65259f520c
yowzah <nagiev2002@bk.ru>
2021-09-14 00:54:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ bot.launch()
 // Set telegram webhook
 // The second argument is necessary only if the client uses a self-signed
 // certificate. Including it for a verified certificate may cause things to break.
-bot.telegram.setWebhook('https://n078.herokuapp.com/secret-path')
-
+bot.telegram.setWebhook('https://n078.herokuapp.com/secret-path', {
+  source: 'server-cert.pem'
+})
 
 // Http webhook, for nginx/heroku users.
 bot.startWebhook('/secret-path', null, 5000)
