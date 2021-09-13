6ba46f1828a954677896412e032f639fe185e9a0
yowzah <nagiev2002@bk.ru>
2021-09-14 00:25:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,5 @@ const fs = require('fs')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.telegram.setWebhook('https://n078.herokuapp.com/', {
-  source: 'server-cert.pem'
-})
-
-// Http webhook, for nginx/heroku users.
+bot.telegram.setWebhook('https://server.tld:8443/secret-path')
 bot.startWebhook('/secret-path', null, 5000)
