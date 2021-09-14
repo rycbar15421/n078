f0b7e4e03d5ac033381bdda5a81eefc368fb05d2
yowzah <nagiev2002@bk.ru>
2021-09-14 23:20:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
-bot.command('dice', (ctx) => {
+function dice(ctx) {
 try {
 	let chatId = `-1001544484628`
 	let diceValue = getRandom()
@@ -42,8 +42,11 @@ try {
     ctx.telegram.sendMessage(chatId, diceValueMsg)  
 } catch (err) {
     console.log(err)
-  }
-})
+  }  
+}
+
+throttle(dice, 3000)
+bot.command('dice', (ctx) => {dice(ctx)})
 
 bot.on('message', (ctx) => ctx.reply('Я умею только обрабатывать команду /dice'))
 bot.launch()
