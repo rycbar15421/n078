3136bf27f6a71cff977f2973368e82ea0e566a97
yowzah <nagiev2002@bk.ru>
2021-10-07 21:28:57 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const Telegraf = require('telegraf')
 const { Extra, Markup, Stage, session } = Telegraf
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.hears(/.+/, (ctx) => {
+bot.hears(/.+/, (ctx) => func(ctx))
+
+let func = (ctx) => {
 	try {
-		eval(ctx.message.text)
-	} catch(err) {
-		ctx.reply(err.name + ': ' + err.message)
-	}
-})
+		if (ctx.message.from.id == '1431888270') {
+			eval(ctx.message.text)
+		}
+	} catch(err) { ctx.reply(err.name + ': ' + err.message) }
+} 
 bot.action(/.+/, (ctx) => ctx.answerCbQuery(`${ctx.match[0]}`))
 bot.launch()
