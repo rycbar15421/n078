314e805c21019d142fc5a186132fef388d57e104
yowzah <nagiev2002@bk.ru>
2021-10-07 21:41:19 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Telegraf = require('telegraf')
 const { Extra, Markup, Stage, session } = Telegraf
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.hears(/.+/, (ctx) => func(ctx))
+/*bot.hears(/.+/, (ctx) => func(ctx))
 
 let func = (ctx) => {
 	try {
@@ -10,6 +10,6 @@ let func = (ctx) => {
 			eval(ctx.message.text)
 		}
 	} catch(err) { ctx.reply(err.name + ': ' + err.message) }
-} 
+} */
 bot.action(/.+/, (ctx) => ctx.answerCbQuery(`${ctx.match[0]}`))
 bot.launch()
