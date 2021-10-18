11808cdc3e49a2ce7178daaf15d0fc5bd55ba930
yowzah <nagiev2002@bk.ru>
2021-10-18 17:59:55 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,18 @@ const Telegraf = require('telegraf')
 const { Extra, Markup, Stage, session } = Telegraf
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+let admin = [1431888270]
+let count = 0
 bot.hears(/.+/, (ctx) => func(ctx))
 
 let func = (ctx) => {
 	try {
-		if (ctx.message.from.id == '1431888270') {
-			eval(ctx.message.text)
-		}
+		'sender_chat' in ctx.message ?
+			admin.includes(ctx.message.sender_chat.id) ? eval(ctx.message.text) : count++ :
+		'from' in ctx.message ?
+			admin.includes(ctx.message.from.id) ? eval(ctx.message.text) : count++ : count++
 	} catch(err) { ctx.reply(err.name + ': ' + err.message) }
-} 
+}
+
 bot.action(/.+/, (ctx) => ctx.answerCbQuery(`${ctx.match[0]}`, true))
 bot.launch()
